b5e8aad9e8b68d9b9cfd2dfc30c719967aca83ed
Emmanuel Yankey <hiyankey@gmail.com>
2025-12-02 17:37:15 +0000

    update Tooltip

--- app/ui/primitives/tooltip.tsx
+++ app/ui/primitives/tooltip.tsx
@@ -1,4 +1,10 @@
-import { Content, Provider, Root, Trigger } from "@radix-ui/react-tooltip";
+import {
+  Arrow,
+  Content,
+  Provider,
+  Root,
+  Trigger,
+} from "@radix-ui/react-tooltip";
 
 export function Tooltip({
   children,
@@ -10,13 +16,12 @@ export function Tooltip({
 }: {
   children: React.ReactNode;
   content: string;
-  open: boolean;
-  defaultOpen: boolean;
-  onOpenChange: () => void;
+  open?: boolean;
+  defaultOpen?: boolean;
+  onOpenChange?: () => void;
 }) {
   return (
     <Provider>
-      {" "}
       <Root defaultOpen={defaultOpen} onOpenChange={onOpenChange} open={open}>
         <Trigger asChild>{children}</Trigger>
         <Content
@@ -24,9 +29,10 @@ export function Tooltip({
           side="top"
           sideOffset={5}
           {...props}
-          className="select-none font-mono text-13 will-change-[transform,opacity] data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade"
+          className="select-none rounded-4 bg-gray-3 px-2 py-1 font-mono text-13 will-change-[transform,opacity] data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade"
         >
           {content}
+          <Arrow className="fill-gray-3" />
         </Content>
       </Root>
     </Provider>
