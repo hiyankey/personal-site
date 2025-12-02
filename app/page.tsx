77313aac87ca8add76ea190f6371470faf91b96d
Emmanuel Yankey <hiyankey@gmail.com>
2025-12-02 17:38:47 +0000

    bleed grid container

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import { Tooltip } from "./ui/primitives/tooltip";
 
 export default function Home() {
   const { dark, toggleDark } = useTheme();
+
   return (
     <div className="flex min-h-dvh flex-col">
       <Tooltip content="&#8997; + T">
@@ -18,18 +19,19 @@ export default function Home() {
         </button>
       </Tooltip>
 
-      <div className="mx-auto mb-24 max-w-150 pt-24 leading-7">
+      <div className="mx-auto mb-16 max-w-150 space-y-4 pt-24 leading-7">
         <p>
           My name is Emmanuel, I’m an Interactive designer for Web/Products,
           Crafting details, Things for screens
         </p>
+        <p>Focused on honing my craft</p>
         <p>When not coding, I do endurance things</p>
       </div>
       <div className="mx-auto mb-24 w-full max-w-150 leading-7">
-        <h3 className="mb-6 font-mono text-14 text-gray-9">Timeline</h3>
+        <h3 className="mb-4 font-mono text-14 text-gray-11">Timeline</h3>
         <ul className="group flex flex-col space-y-1">
           <li className="relative flex h-14 w-full items-center space-x-3 transition-[padding] duration-300 hover:pl-3">
-            <span className="font-mono text-14 text-gray-9">2025</span>
+            <span className="font-mono text-14 text-gray-11">2025</span>
             <div className="gridLine relative! inline-flex w-full" />
             <Link className="inline-flex text-nowrap" href={"#"}>
               Devouring Details
@@ -37,7 +39,7 @@ export default function Home() {
             </Link>
           </li>
           <li className="relative flex h-14 w-full items-center space-x-3 transition-[padding] duration-300 hover:pl-3">
-            <span className="font-mono text-14 text-gray-9">2025</span>
+            <span className="font-mono text-14 text-gray-11">2025</span>
             <div className="gridLine relative! inline-flex w-full" />
             <Link className="inline-flex text-nowrap" href={"#"}>
               Animations on the Web <span className="absolute inset-0" />
@@ -45,47 +47,68 @@ export default function Home() {
           </li>
         </ul>
       </div>
-      <div className="relative h-120 translate-center-x  max-w-240">
-        <div className=" absolute inset-0 mx-auto h-120 max-w-240 overflow-clip">
-          <Card className="top-4 left-64" height={185.27} width={322}>
-            <Image
-              alt=""
-              className="h-full w-full"
-              fill
-              src={"/test-image-2.jpg"}
-            />
-          </Card>
-          <Card className="top-11 left-[60%]" height={188} width={260}>
-            <ElasticSlider />
-          </Card>
-          <Card className="-top-36 left-[6%]" height={242} width={422}>
-            <Image
-              alt=""
-              className="h-full w-full "
-              fill
-              src={"/test-image-1.jpg"}
-            />
-          </Card>
-        </div>
-        <div className="-z-10 absolute inset-0 mx-auto h-120 max-w-240">
-          <Grid columns={48} rows={24}>
-            <Grid.Cross column={1} row={1}>
-              <Cross2Icon className="-translate-y-1/2 -translate-x-1/2 size-5 rotate-45" />
-            </Grid.Cross>
-            <Grid.Cross column={-2} row={1}>
-              <Cross2Icon className="-translate-y-1/2 size-5 translate-x-1/2 rotate-45" />
-            </Grid.Cross>
-            <Grid.Cross column={1} row={-2}>
-              <Cross2Icon className="-translate-x-1/2 size-5 translate-y-1/2 rotate-45" />
-            </Grid.Cross>
-            <Grid.Cross column={-2} row={-2}>
-              <Cross2Icon className="size-5 translate-x-1/2 translate-y-1/2 rotate-45" />
-            </Grid.Cross>
-          </Grid>
+      <div className="bleed">
+        <div className="translate-center-x relative h-120 w-full max-w-240">
+          <div className="absolute inset-0 h-full w-full overflow-clip">
+            <Card
+              className="absolute top-4 left-64"
+              height={185.27}
+              title="Test Image 1"
+              width={322}
+            >
+              <Image
+                alt="test image"
+                className="h-full w-full object-cover"
+                fill
+                src={"/test-image-2.jpg"}
+              />
+            </Card>
+            <Card
+              className="absolute top-11 left-[60%]"
+              height={188}
+              title="Elastic Slider"
+              width={260}
+            >
+              <ElasticSlider />
+            </Card>
+            <Card
+              className="-top-36 absolute left-[6%]"
+              height={242}
+              title="Test Image 2"
+              width={422}
+            >
+              <Image
+                alt="test image"
+                className="h-full w-full object-cover"
+                fill
+                src={"/test-image-1.jpg"}
+              />
+            </Card>
+          </div>
+          <div
+            className="-z-10 sm:data-[columns=48] sm:data-[rows=24] data-[columns=24] data-[rows=12] absolute inset-0 h-full w-full"
+            data-columns={48}
+            data-rows={24}
+          >
+            <Grid columns={48} rows={24}>
+              <Grid.Cross column={1} row={1}>
+                <Cross2Icon className="-translate-y-1/2 -translate-x-1/2 size-5 rotate-45" />
+              </Grid.Cross>
+              <Grid.Cross column={-2} row={1}>
+                <Cross2Icon className="-translate-y-1/2 size-5 translate-x-1/2 rotate-45" />
+              </Grid.Cross>
+              <Grid.Cross column={1} row={-2}>
+                <Cross2Icon className="-translate-x-1/2 size-5 translate-y-1/2 rotate-45" />
+              </Grid.Cross>
+              <Grid.Cross column={-2} row={-2}>
+                <Cross2Icon className="size-5 translate-x-1/2 translate-y-1/2 rotate-45" />
+              </Grid.Cross>
+            </Grid>
+          </div>
         </div>
       </div>
 
-      <div className="mx-auto my-8 font-mono text-14 text-gray-9">
+      <div className="mx-auto my-8 font-mono text-14 text-gray-11">
         20251201_121913.976_UTC
       </div>
     </div>
--- app/ui/card.tsx
+++ app/ui/card.tsx
@@ -1,34 +1,75 @@
-import { Cross2Icon } from "@radix-ui/react-icons";
+"use client";
+import { CornerBottomRightIcon, Cross2Icon } from "@radix-ui/react-icons";
+import { motion, useDragControls } from "motion/react";
+import { useState } from "react";
 
 type CardProps = {
   children?: React.ReactNode;
   height: number;
   width: number;
   className?: string;
-  handleClick?: () => void;
+  title: string;
 };
-export function Card({
-  children,
-  width,
-  height,
-  className,
-  handleClick,
-}: CardProps) {
+export function Card({ children, width, height, className, title }: CardProps) {
+  const [isDragging, setIsDragging] = useState(false);
+  const dragControls = useDragControls();
+  const startDrag = (event: React.PointerEvent) => {
+    dragControls.start(event);
+  };
   return (
-    <div
-      className={`${"relative rounded-6 bg-gray-3/90 p-1 pt-6 shadow-border backdrop-blur-[20px]"} ${className}`}
-      style={{ height, width }}
+    <motion.div
+      className={`${"relative rounded-6 bg-gray-3/90 p-1 shadow-[var(--shadow-border),var(--shadow-large)] backdrop-blur-[20px]"} ${className}`}
+      drag
+      dragControls={dragControls}
+      dragListener={false}
+      dragMomentum={false}
+      initial={"default"}
+      onDragEnd={() => setIsDragging(false)}
+      onDragStart={() => setIsDragging(true)}
+      style={{ height, width, zIndex: isDragging ? 10 : 0 }}
+      transition={{ ease: "easeOut" }}
+      variants={{
+        hovered: { paddingTop: 24 },
+        default: { paddingTop: 4 },
+      }}
+      whileHover={"hovered"}
     >
-      <button
+      <div
+        className="absolute top-0 right-0 left-0 inline-block cursor-grab p-6"
+        onPointerDown={startDrag}
+      />
+      <motion.h3
+        className="absolute top-1 left-1 text-12"
+        variants={{
+          hovered: { opacity: 1, scale: 1 },
+          default: { opacity: 0, scale: 0.9 },
+        }}
+      >
+        {title}
+      </motion.h3>
+      <motion.button
         className="absolute top-1 right-1"
-        onClick={handleClick}
         type="button"
+        variants={{
+          hovered: { opacity: 1, scale: 1 },
+          default: { opacity: 0, scale: 0 },
+        }}
       >
         <Cross2Icon />
-      </button>
-      <div className="relative  flex h-full w-full flex-col overflow-clip rounded-4 bg-gray-2 p-1">
+      </motion.button>
+      <div className="relative flex h-full w-full touch-none select-none flex-col overflow-clip rounded-4 bg-(--canvas) p-1">
         {children}
       </div>
-    </div>
+      <motion.button
+        className="absolute right-0 bottom-0"
+        type="button"
+        variants={{
+          hovered: { opacity: 1, scale: 1 },
+          default: { opacity: 0, scale: 0 },
+        }}
+      >
+        <CornerBottomRightIcon />
+      </motion.button>
+    </motion.div>
   );
 }
